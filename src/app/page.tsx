d03c459ae5c1062723257b9c4a5cfa40a0316c5c
rexh1002 <wjdgycjs@gmail.com>
2025-04-12 18:21:00 +0900

    fix: 웹/모바일 하단 버튼 여백 개별 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -575,7 +575,7 @@ export default function HomePage() {
             </section>
 
             {/* 선택사항 적용 버튼 */}
-            <div className="sticky bottom-0 left-0 right-0 bg-white p-4 border-t mt-8 mb-24 sm:mb-0">
+            <div className="sticky bottom-0 left-0 right-0 bg-white p-4 border-t mt-8 mb-32 sm:mb-4">
               <div className="flex justify-end space-x-2 max-w-screen-xl mx-auto">
                 <button
                   onClick={clearSelections}
