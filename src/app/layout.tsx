1097e31ce949eed4d433494c54f5194cedbf5afc
rexh1002 <wjdgycjs@gmail.com>
2025-04-30 23:54:19 +0900

    chore: 다크/라이트 모드 기본값을 라이트로 설정 (ThemeProvider defaultTheme=light)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} ${notoSansKr.variable} font-noto-sans-kr antialiased`}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
       </body>
