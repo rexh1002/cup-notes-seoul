0b3c7de24c0a4227bd302768d5ae8bea380a488f
rexh1002 <wjdgycjs@gmail.com>
2025-05-27 18:07:37 +0900

    style: Change '목록보기' to '목록 보기'

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -601,7 +601,7 @@ const Map = forwardRef<MapHandle, MapProps>(function Map({
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
           </svg>
-          목록보기
+          목록 보기
         </button>
       </div>
 
